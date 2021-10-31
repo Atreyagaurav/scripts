156b6546ae311611351a4708be7f0278e775955d
Gaurav <allmanpride@gmail.com>
2021-10-31 15:46:31 -0400

    Add conky.sh

---
+++ conky.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+conky -c ~/.config/conky/conky_left.conf -d &
+
+~/kool/programming/c/birthdays/birthdays_final -u7 > /tmp/bdays.txt
+~/kool/programming/c/birthdays/birthdays_final -d > /tmp/today.txt
+
+conky -c ~/.config/conky/conky_lb.conf -d &
+conky -c ~/.config/conky/conky_rb.conf -d &
+conky -c ~/.config/conky/conky_bottom.conf -d &
+conky -c ~/.config/conky/conky_right.conf -d &
+~/scripts/daily_for_conky.sh 
+exit
---
+++ daily_for_conky.sh
@@ -0,0 +1,7 @@
+
+#!/usr/bin/env bash
+~/kool/programming/c/birthdays/birthdays_final -u7 > /tmp/bdays.txt
+~/kool/programming/c/birthdays/birthdays_final -d > /tmp/today.txt
+~/kool/programming/c/calendar-desktop/calender-desktop > /tmp/calendar.txt 
+# python ~/scripts/corona/summary.py
+# ~/scripts/plot_corona.sh
