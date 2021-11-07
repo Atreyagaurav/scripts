9d8d86bdb6baef15773657de0be3b4218b293b8c
Gaurav <allmanpride@gmail.com>
2021-11-06 21:28:01 -0400

    Add notification for brightness change

--- brightness.sh
+++ brightness.sh
@@ -29,5 +29,6 @@ then
 fi
 
 VAL=$(($VAL_MAX*$PERC/100))
-echo $VAL>/sys/class/backlight/intel_backlight/brightness
-echo $PERC>/tmp/brightness
+# && makes it so that the notification.sh is not sent if the operation
+# fails
+echo $VAL>/sys/class/backlight/intel_backlight/brightness && notify-send brightness $PERC% -h int:value:$PERC -i /usr/share/icons/Adwaita/32x32/status/display-brightness-symbolic.symbolic.png -h string:x-dunst-stack-tag:brightness --urgency=LOW && echo $PERC>/tmp/brightness
