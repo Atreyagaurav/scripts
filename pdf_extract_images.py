1237227b85a6f161ea29baafe70280dc503269e4
Gaurav <allmanpride@gmail.com>
2022-01-31 17:23:31 -0500

    Pdf_extract image skip imageless pages.

--- pdf_extract_images.py
+++ pdf_extract_images.py
@@ -6,20 +6,23 @@ import io
 def extimg(location, pdffile):
     pf = fitz.open(os.path.join(location, pdffile))
     for pi in range(len(pf)):
-        iml = pf[pi].getImageList()[0][0] #image index of page, xml ref string is in [0]
-        im = pf.extractImage(iml)
-        image_bytes = im["image"]
-        # get the image extension
-        image_ext = im["ext"]
-        # load it to PIL
-        image = Image.open(io.BytesIO(image_bytes))
-        # save it to local disk
-        fn = os.path.join('/tmp/images',location, pdffile)
-        fn = f'{"".join(fn.split(".")[:-1])}/{pi+1}.{image_ext}'
-        print(fn)
-        os.makedirs(os.path.dirname(fn),exist_ok=True)
-        with open(fn, "wb") as w:
-            image.save(w)
+        iml = pf[pi].getImageList()
+        if not iml:
+            continue
+        for i in range(len(iml)):
+            im = pf.extractImage(iml[i][0]) #image index of page, xml ref string is in [0]
+            image_bytes = im["image"]
+            # get the image extension
+            image_ext = im["ext"]
+            # load it to PIL
+            image = Image.open(io.BytesIO(image_bytes))
+            # save it to local disk
+            fn = os.path.join('/tmp/images',location, pdffile)
+            fn = f'{"".join(fn.split(".")[:-1])}/{pi+1}_{i}.{image_ext}'
+            print(fn)
+            os.makedirs(os.path.dirname(fn),exist_ok=True)
+            with open(fn, "wb") as w:
+                image.save(w)
 
 def map_files(path = '.'):
     for root, dirs, files in os.walk(path):
