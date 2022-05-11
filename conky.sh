2b818d259633ac1c04f0e8aa2b88958df01c2056
Gaurav-Lab-PC <atreyagv@mail.uc.edu>
2022-05-11 17:28:39 -0400

    Update readme; add remind conky

--- conky.sh
+++ conky.sh
@@ -8,5 +8,6 @@ conky -c ~/.config/conky/conky_lb.conf -d &
 conky -c ~/.config/conky/conky_rb.conf -d &
 conky -c ~/.config/conky/conky_bottom.conf -d &
 conky -c ~/.config/conky/conky_right.conf -d &
+conky -c ~/.config/conky/conky_2ndmon.conf -d &
 ~/scripts/daily_for_conky.sh 
 exit
