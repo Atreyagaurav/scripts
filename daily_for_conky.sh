c1a1f79f2f13f02218a34fcc1cb22a9e66de9bf1
Gaurav <allmanpride@gmail.com>
2022-09-17 18:05:07 -0400

    Add option to clean residue latex files

--- daily_for_conky.sh
+++ daily_for_conky.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 ~/github/birthdays-reminder/birthdays -u7 > /tmp/bdays.txt
 ~/github/birthdays-reminder/birthdays -d > /tmp/today.txt
-~/github/calendar-desktop/calender-desktop > /tmp/calendar.txt 
+remind -s ~/.reminder/ | grep `date +"%Y/%m/%d"` | awk '{x=$3;$1=$2=$3=$4=$5="";$0=$0;$1=$1; print "["x"] "$0}' | ~/github/calendar-desktop/calender-desktop > /tmp/calendar.txt 
 ~/github/birthdays-reminder/birthdays -r > ~/.reminder/40-birthdays.rem
 # python ~/scripts/corona/summary.py
 # ~/scripts/plot_corona.sh
